b289e586f7c36456bb453661ee7f5d33acc065d9
Mr Nim <50592567+mrnim94@users.noreply.github.com>
2025-09-25 06:44:43 +0000

    update data azurerm_kubernetes_service_versions

--- c3-aks.tf
+++ c3-aks.tf
@@ -1,4 +1,11 @@
+# Documentation Reference: https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/data-sources/kubernetes_service_versions
 
+# Datasource to get Latest Azure AKS latest Version
+
+data "azurerm_kubernetes_service_versions" "current" {
+ location = var.resource_group_location
+ include_preview = false
+}
 
 resource "azurerm_kubernetes_cluster" "aks_cluster" {
   name                = var.cluster_name
